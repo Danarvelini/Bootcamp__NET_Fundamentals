a62d4a93d7ac5b258a7ab1772267cdb29dabb237
DanHausen <danihausen@live.com>
2021-07-06 18:53:46 -0300

    main

--- Intro_to_C#/ex2.cs
+++ Intro_to_C#/ex2.cs
@@ -5,3 +5,12 @@
 
 // O seu desafio é ler a distância (em Km) e calcular quanto tempo leva (em minutos) para a moto Y tomar essa distância da outra moto.
 
+using System;
+
+class Desafio {
+    static void Main() {
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+        Console.WriteLine(minutos + " minutos");
+    }
+}
