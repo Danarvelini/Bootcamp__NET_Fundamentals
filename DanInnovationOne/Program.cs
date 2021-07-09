da97f7244a84051df94c2e65656d18ee7d5a5446
danhausen <danihausen@live.com>
2021-07-09 14:39:48 -0300

    main

---
+++ DanInnovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DanInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int teste = 0;
+            while(teste <= 100){
+                Console.WriteLine(teste);
+                teste++;
+            }
+        }
+    }
+}
