f84c9394d2f83c4d5f33399feb7cb95854f4a299
DanHausen <danihausen@live.com>
2021-07-21 17:58:40 -0300

    main

--- DIO.Bank/Program.cs
+++ DIO.Bank/Program.cs
@@ -6,7 +6,54 @@ namespace DIO.Bank
     {
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 150, 0, "Daniel");
+            string opcaoUsuario = ObterOpcaoUsuario();
+            
+            while (opcaoUsuario.ToUpper() != "X"){
+                switch (opcaoUsuario){
+                    case "1":
+                        //ListarContas();
+                        break;
+                    case "2":
+                        //InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+            Console.ReadLine();
+        }
+        
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine("");
+            Console.WriteLine("DanBank ao seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada:");
+            
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine("");
+            
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine("");
+            return opcaoUsuario;
         }
     }
 }
